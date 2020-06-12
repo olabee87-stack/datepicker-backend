d3c97f7d7e6f0f732ce1aee3c2cb5c019fee5063
veronika333 <59805610+veronika333@users.noreply.github.com>
2020-06-12 14:56:16 +0300

    changed the type of likes in schema

--- api/models/event.js
+++ api/models/event.js
@@ -12,7 +12,7 @@ const eventSchema = new Schema({
   title: { type: String, required: true },
   description: { type: String, required: true },
   date: { type: Date, required: true },
-  likes: [{ type: Array }]
+  likes: { type: Number }
 });
 
 const Event = mongoose.model("Event", eventSchema);
